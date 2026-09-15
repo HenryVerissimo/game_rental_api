22f47752f6d193d1b97d288533326c130eecbfe7
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-15 19:42:31 -0300

    feat(models): add User entity
    - Represent user data in the core layer with properties and type validations.

---
+++ src/GameRentalApi.Core/Models/User.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GameRentalApi.Api.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; } = null;
+    public DateTime? DeletedAt { get; set; } = null;
+
+    public ICollection<UserRole> UserRoles { get; set; }= new List<UserRole>();
+    public ICollection<Rental> Rentals { get; set; } = new List<Rental>();
+}
