12be8b9381d281cbad7a2d3cc80cce646324d3a1
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-15 19:41:17 -0300

    feat(models): add Rental entity
    - Represent rental data in the core layer with properties and type validations.

---
+++ src/GameRentalApi.Core/Models/Rental.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameRentalApi.Api.Models;
+
+
+public class Rental
+{
+    public int Id { get; set; }
+    public decimal TotalAmount { get; set; }
+    public DateTime RentalDate { get; set; } = DateTime.UtcNow;
+    public DateTime Deadline { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+
+    public int UserId { get; set; }
+    public User User {get; set; } = null!;
+    public ICollection<RentalGame> RentalGames { get; set; }= new List<RentalGame>();
+}
