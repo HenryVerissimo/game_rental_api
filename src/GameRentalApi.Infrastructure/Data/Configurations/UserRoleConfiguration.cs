059f0cc3c9c9c15ee7e46b9062eefdfad3ad6f36
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-16 16:49:58 -0300

    feat(db): add User entity configuration
    - mplement IEntityTypeConfiguration
    - Define database constraints, keys, and table mappings for User

---
+++ src/GameRentalApi.Infrastructure/Data/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,24 @@
+using GameRentalApi.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameRentalApi.Infrastructure.Data.Configurations;
+
+
+public class UserRoleConfiguraton : IEntityTypeConfiguration<UserRole>
+{
+    public void Configure(EntityTypeBuilder<UserRole> builder)
+    {
+        builder.HasKey(userRole => new { userRole.UserId, userRole.RoleId });
+
+        builder.HasOne(userRole => userRole.User)
+            .WithMany(user => user.UserRoles)
+            .HasForeignKey(userRole => userRole.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(userRole => userRole.Role)
+            .WithMany(role => role.UserRoles)
+            .HasForeignKey(userRole => userRole.RoleId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
