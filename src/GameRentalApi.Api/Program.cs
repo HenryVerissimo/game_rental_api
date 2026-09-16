9dbce99125fa2a8621703803a3aa12562918aaaf
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-16 15:53:53 -0300

    feat(infra): create AppDbContext and configure database connection
    - Create AppDbContext mapping all domain entities.
    - Configure database connection string  and driver in Program.cs.

--- src/GameRentalApi.Api/Program.cs
+++ src/GameRentalApi.Api/Program.cs
@@ -1,4 +1,20 @@
+using DotNetEnv;
+
+using GameRentalApi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
+builder.Configuration.AddEnvironmentVariables();
+var connectionString =
+    builder.Configuration.GetConnectionString("DB_CONNECTION_STRING")
+    ?? Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 app.Run();
---
+++ src/GameRentalApi.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using GameRentalApi.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameRentalApi.Infrastructure.Data;
+
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    public DbSet<Game> Games { get; set; }
+    public DbSet<Rental> Rentals { get; set; }
+    public DbSet<RentalGame> RentalGames { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<VideoGame> VideoGames { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
