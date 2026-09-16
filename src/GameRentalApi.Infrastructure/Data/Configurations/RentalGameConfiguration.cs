206616fc4e1a94d932ec481b9dd2d14db1291d07
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-16 16:19:50 -0300

    feat(db): add RentalGame entity configuration
    - Implement IEntityTypeConfiguration<RentalGame>
    - Define database constraints, keys, and table mappings for RentalGame

---
+++ src/GameRentalApi.Infrastructure/Data/Configurations/RentalGameConfiguration.cs
@@ -0,0 +1,27 @@
+using GameRentalApi.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameRentalApi.Infrastructure.Data.Configurations;
+
+
+public class RentalGameConfiguration : IEntityTypeConfiguration<RentalGame>
+{
+    public void Configure(EntityTypeBuilder<RentalGame> builder)
+    {
+        builder.HasKey(rentalGame => new { rentalGame.RentalId, rentalGame.GameId});
+
+        builder.Property(rentalGame => rentalGame.UnitGamePrice)
+            .HasPrecision(18, 2);
+
+        builder.HasOne(rentalGame => rentalGame.Rental)
+            .WithMany(rental => rental.RentalGames)
+            .HasForeignKey(rentalGame => rentalGame.RentalId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(rentalGame => rentalGame.Game)
+            .WithMany(game => game.RentalGames)
+            .HasForeignKey(rentalGame => rentalGame.GameId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
