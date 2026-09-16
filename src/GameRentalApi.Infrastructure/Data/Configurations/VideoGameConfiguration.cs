13153d9d12676249816fb74525d8faa87a802910
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-16 16:50:54 -0300

    feat(db): add VideoGame entity configuration
    - Implement IEntityTypeConfiguration
    
    - Define database constraints and key  for VideoGame

---
+++ src/GameRentalApi.Infrastructure/Data/Configurations/VideoGameConfiguration.cs
@@ -0,0 +1,22 @@
+using GameRentalApi.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameRentalApi.Infrastructure.Data.Configurations;
+
+
+public class VideoGameConfiguration : IEntityTypeConfiguration<VideoGame>
+{
+    public void Configure(EntityTypeBuilder<VideoGame> builder)
+    {
+        builder.HasKey(videoGame => videoGame.Id);
+
+        builder.Property(videoGame => videoGame.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(videoGame => videoGame.Company)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
