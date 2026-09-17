9294eca4060974fb03b05a6016d36e6b670b4876
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-17 16:38:07 -0300

    feat(migrations): add AddDeletedAtColumnInRole migration

---
+++ src/GameRentalApi.Infrastructure/Migrations/20260917193605_AddDeletedAtColumnInRole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameRentalApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeletedAtColumnInRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Roles",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Roles");
+        }
+    }
+}
