7ddcc250134d890aaf29b2bce43462540d7b34f0
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-22 20:13:48 -0300

    feat(dto): add RoleResponseDTO
    - Implement new DTO to structure outgoing role data in the API

---
+++ src/GameRentalApi.Core/DTOs/RoleResponseDTO.cs
@@ -0,0 +1,9 @@
+namespace GameRentalApi.Core.DTOs;
+
+
+public class RoleResponseDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;  
+}
