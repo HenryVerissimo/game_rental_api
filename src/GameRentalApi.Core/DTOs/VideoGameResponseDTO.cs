631e53cc2937ab0316b5f29654e4ca5ee801507d
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-22 20:14:54 -0300

    feat(dto): add VideoGameResponseDTO
    - Implement new DTO to structure outgoing videoGame data in the API

---
+++ src/GameRentalApi.Core/DTOs/VideoGameResponseDTO.cs
@@ -0,0 +1,9 @@
+namespace GameRentalApi.Core.DTOs;
+
+
+public class VideoGameResponseDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Company { get; set; } = string.Empty; 
+}
