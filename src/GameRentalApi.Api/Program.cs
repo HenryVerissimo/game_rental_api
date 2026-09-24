f813b8946de3e38932eb8331bf803f49634aac30
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-09-24 17:46:33 -0300

    feat(api): configure OpenAPI documentation and controllers
    - Configure controller services and OpenAPI documentation in Program.cs, including Swagger UI integration.

--- src/GameRentalApi.Api/Program.cs
+++ src/GameRentalApi.Api/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
 
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 builder.Configuration.AddEnvironmentVariables();
 var connectionString =
     builder.Configuration.GetConnectionString("DB_CONNECTION_STRING")
@@ -17,12 +19,24 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddScoped<IGameRepository, GameRepository>();
 builder.Services.AddScoped<IRentalGameRepository, RentalGameRepository>();
-builder.Services.AddScoped<IRentalRepository, IRentalRepository>();
+builder.Services.AddScoped<IRentalRepository, RentalRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserRoleRepository, UserRoleRepository>();
 builder.Services.AddScoped<IVideoGameRepository, VideoGameRepository>();
 
 var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Rental Games API");
+    });
+}
+
+app.MapControllers();
 
 app.Run();
